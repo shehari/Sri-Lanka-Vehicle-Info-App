e4f38e521e7a2ff875f9942ffb435a99f6103d1a
HansikaPerera <madhuhanz@gmail.com>
2020-09-30 18:16:46 +0530

    Added validations to Sign In

--- app/src/main/java/com/example/srilankavehicleinfo/LoginPage.java
+++ app/src/main/java/com/example/srilankavehicleinfo/LoginPage.java
@@ -2,11 +2,14 @@ package com.example.srilankavehicleinfo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,33 +17,46 @@ public class LoginPage extends AppCompatActivity {
     private TextView signUpNav;
     private Button login;
     private TextView pwReset;
+    private EditText userName, password;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_page);
 
+        userName = (EditText) findViewById(R.id.user_name);
+        password = (EditText) findViewById(R.id.pw);
         signUpNav = (TextView) findViewById(R.id.txt_navSignUp);
+        pwReset = (TextView) findViewById(R.id.txt_navPW);
+        login = (Button) findViewById(R.id.btn_login);
+
         signUpNav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openSignUp();
             }
         });
-
-        pwReset = (TextView) findViewById(R.id.txt_navPW);
         pwReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openPWReset();
             }
         });
-
-        login = (Button) findViewById(R.id.btn_login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openMain();
+
+                if (TextUtils.isEmpty(userName.getText().toString())) {
+                    Toast.makeText(LoginPage.this, "User Name cannot be Empty..", Toast.LENGTH_SHORT).show();
+                }
+
+                else if (TextUtils.isEmpty(password.getText().toString())) {
+                    Toast.makeText(LoginPage.this, "Password cannot be Empty..", Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    openMain();
+                }
             }
         });
     }
