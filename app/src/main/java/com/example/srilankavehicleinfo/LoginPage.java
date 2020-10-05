71d81360430677a4349740194b31d269eefe8b58
HansikaPerera <madhuhanz@gmail.com>
2020-10-05 21:56:01 +0530

    Login password Hide and Unhide Updated.

--- app/src/main/java/com/example/srilankavehicleinfo/LoginPage.java
+++ app/src/main/java/com/example/srilankavehicleinfo/LoginPage.java
@@ -3,21 +3,29 @@ package com.example.srilankavehicleinfo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.regex.Pattern;
+
 public class LoginPage extends AppCompatActivity {
     private TextView signUpNav;
     private Button login;
     private TextView pwReset;
     private EditText userName, password;
+    private ImageView PWeye;
+
+    private static final Pattern password_Pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{6,}$");
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +37,7 @@ public class LoginPage extends AppCompatActivity {
         signUpNav = (TextView) findViewById(R.id.txt_navSignUp);
         pwReset = (TextView) findViewById(R.id.txt_navPW);
         login = (Button) findViewById(R.id.btn_login);
+        PWeye = (ImageView) findViewById(R.id.PW_eye);
 
         signUpNav.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,22 +45,30 @@ public class LoginPage extends AppCompatActivity {
                 openSignUp();
             }
         });
+
         pwReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openPWReset();
             }
         });
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (TextUtils.isEmpty(userName.getText().toString())) {
-                    Toast.makeText(LoginPage.this, "User Name cannot be Empty..", Toast.LENGTH_SHORT).show();
+                String NameInput = userName.getText().toString().trim();
+                String PasswordInput = password.getText().toString().trim();
+
+                if (NameInput.isEmpty()) {
+                    userName.setError("Field Can't be Empty");
                 }
 
-                else if (TextUtils.isEmpty(password.getText().toString())) {
-                    Toast.makeText(LoginPage.this, "Password cannot be Empty..", Toast.LENGTH_SHORT).show();
+                else if (PasswordInput.isEmpty()) {
+                    password.setError("Field Can't be Empty");
+                }
+                else if(!password_Pattern.matcher(PasswordInput).matches()){
+                    password.setError("Password is too Week");
                 }
 
                 else {
@@ -59,6 +76,26 @@ public class LoginPage extends AppCompatActivity {
                 }
             }
         });
+
+        PWeye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (PWeye.getTag() != null && PWeye.getTag().toString().equals("eye")) {
+                    PWeye.setImageResource(R.drawable.hide_eye);
+                    PWeye.setTag("hide_eye");
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+
+                else if (PWeye.getTag() != null && PWeye.getTag().toString().equals("hide_eye")) {
+                    PWeye.setImageResource(R.drawable.eye);
+                    PWeye.setTag("eye");
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+
+            }
+        });
+
     }
 
     public void openSignUp(){
