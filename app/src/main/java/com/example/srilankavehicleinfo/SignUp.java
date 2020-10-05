cd559231dcaa365e46429a66754137d3b37bf7dc
HansikaPerera <madhuhanz@gmail.com>
2020-10-05 21:19:01 +0530

    Sign Up Password Hide and Unhide Updated

--- app/src/main/java/com/example/srilankavehicleinfo/SignUp.java
+++ app/src/main/java/com/example/srilankavehicleinfo/SignUp.java
@@ -2,12 +2,16 @@ package com.example.srilankavehicleinfo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +23,7 @@ public class SignUp extends AppCompatActivity {
     private TextView signInNav;
     private EditText name, email, phoneNo, password, cnfrmPW;
     private Button register;
+    private ImageView cnfrmPWEye, PWEye;
 
     private static final Pattern password_Pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{6,}$");
 
@@ -34,6 +39,8 @@ public class SignUp extends AppCompatActivity {
         password = (EditText) findViewById(R.id.pw);
         cnfrmPW = (EditText) findViewById(R.id.cnfrm_pw);
         register = (Button) findViewById(R.id.btn_signup);
+        cnfrmPWEye = (ImageView) findViewById(R.id.cnfrmPW_eye);
+        PWEye = (ImageView) findViewById(R.id.PW_eye);
 
         signInNav.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +88,44 @@ public class SignUp extends AppCompatActivity {
                 }
                 else {
                     openSignIn();
+                }
+
+            }
+        });
+
+        PWEye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (PWEye.getTag() != null && PWEye.getTag().toString().equals("eye")) {
+                    PWEye.setImageResource(R.drawable.hide_eye);
+                    PWEye.setTag("hide_eye");
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+
+                else if (PWEye.getTag() != null && PWEye.getTag().toString().equals("hide_eye")) {
+                    PWEye.setImageResource(R.drawable.eye);
+                    PWEye.setTag("eye");
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+
+            }
+        });
+
+        cnfrmPWEye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (cnfrmPWEye.getTag() != null && cnfrmPWEye.getTag().toString().equals("eye")) {
+                    cnfrmPWEye.setImageResource(R.drawable.hide_eye);
+                    cnfrmPWEye.setTag("hide_eye");
+                    cnfrmPW.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+
+                else if (cnfrmPWEye.getTag() != null && cnfrmPWEye.getTag().toString().equals("hide_eye")) {
+                    cnfrmPWEye.setImageResource(R.drawable.eye);
+                    cnfrmPWEye.setTag("eye");
+                    cnfrmPW.setTransformationMethod(PasswordTransformationMethod.getInstance());
                 }
 
             }
